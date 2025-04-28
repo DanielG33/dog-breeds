f914bd57ae61046e52472a8f4a911b66f9cad742
Daniel Guzman <danielguzman9633@gmail.com>
2025-04-28 02:47:21 -0500

    feat: set up topbar navigation & dynamic active state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
+  },
+  {
+    path: 'breeds',
+    loadComponent: () => import('./pages/breeds-list/breeds-list.component').then(c => c.BreedsListComponent)
   }
 ];
