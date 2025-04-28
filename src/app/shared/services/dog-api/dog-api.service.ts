30af5f00a98a11d737907d497fca1c9f99024fa6
Daniel Guzman <danielguzman9633@gmail.com>
2025-04-28 15:37:45 -0500

    feat: create & implement api interceptor (clean res & error handling) + remove logic from service

--- src/app/shared/services/dog-api/dog-api.service.ts
+++ src/app/shared/services/dog-api/dog-api.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { catchError, map, Observable, throwError } from 'rxjs';
-import { BreedImageApiResponse, BreedListApiResponse } from '../../../core/models/dog-api.model';
 import { Breed } from '../../../core/models/breed.model';
 
 @Injectable({
@@ -13,10 +12,10 @@ export class DogApiService {
   private readonly http = inject(HttpClient);
 
   public getBreedsList(): Observable<Breed[]> {
-    return this.http.get<BreedListApiResponse>(environment.API_URL + 'breeds/list/all')
+    return this.http.get<Record<string, string[]>>(environment.API_URL + 'breeds/list/all')
       .pipe(
         map(res => {
-          return Object.entries(res.message).map(([key, val]) => ({
+          return Object.entries(res).map(([key, val]) => ({
             name: key,
             subBreeds: val
           }))
@@ -31,15 +30,6 @@ export class DogApiService {
   public getRandomImage(breed?: string): Observable<string> {
     const path = breed ? `breed/${breed}/images/random` : 'breeds/image/random';
 
-    return this.http.get<BreedImageApiResponse>(environment.API_URL + path)
-      .pipe(
-        map(res => {
-          return res.message;
-        }),
-        catchError(error => {
-          console.error('Error fetching random image: ', error)
-          return throwError(() => 'Error fetching random image')
-        })
-      )
+    return this.http.get<string>(environment.API_URL + path)
   }
 }
