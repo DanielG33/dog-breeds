ad99e184bf836bf22edbdd03a3c27d2625d1eb08
Daniel Guzman <danielguzman9633@gmail.com>
2025-04-27 23:53:32 -0500

    feat: implement (static) nav menu + overall layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LogoComponent } from './shared/components/logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    LogoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
