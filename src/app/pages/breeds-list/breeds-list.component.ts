f4c6e0ae62a7bc4601c9437e46e3b36f2fa3f326
Daniel Guzman <danielguzman9633@gmail.com>
2025-04-28 01:31:16 -0500

    feat: create breeds list page with random breed image aside

---
+++ src/app/pages/breeds-list/breeds-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { DogApiService } from '../../shared/services/dog-api/dog-api.service';
+import { Breed } from '../../core/models/breed.model';
+import { TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-breeds-list',
+  imports: [
+    MatListModule,
+    TitleCasePipe
+  ],
+  templateUrl: './breeds-list.component.html',
+  styleUrl: './breeds-list.component.scss'
+})
+export class BreedsListComponent implements OnInit {
+  private readonly apiService = inject(DogApiService);
+
+  public breeds: Breed[] = [];
+  public randomBreed!: { name: string, subName?: string, path: string[], image: string };
+
+  ngOnInit(): void {
+    this.getBreeds();
+    this.getRandomBreed();
+  }
+
+  private getBreeds() {
+    this.apiService.getBreedsList()
+      .subscribe(breeds => {
+        this.breeds = breeds;
+      })
+  }
+
+  private getRandomBreed() {
+    this.apiService.getRandomImage()
+      .subscribe(url => {
+        const breed = url.split('/')[4];
+        const breedFragments = breed.split('-');
+
+        this.randomBreed = {
+          name: breedFragments[0],
+          subName: breedFragments[1],
+          path: breedFragments,
+          image: url
+        }
+      })
+  }
+}
